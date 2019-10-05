3f4b56e62adf730ea375b4c57329a8f4f8bd53bc
Prateek Gogia <gogia.prateek@hotmail.com>
2019-10-05 16:38:28 +0530

    Deleted Junk Code

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,32 @@
-// var path = require("path");
-
-// module.exports = {
-//   mode: "production",
-//   entry: "./src/index.jsx",
-//   output: {
-//     path: path.resolve("lib"),
-//     filename: "index.js",
-//     libraryTarget: "commonjs2"
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.jsx?$/,
-//         exclude: /(node_modules)/,
-//         use: {
-//           loader: "babel-loader",
-//           options: {
-//             presets: ["@babel/preset-react"]
-//           }
-//         }
-//       },
-//       {
-//         test: /\.css$/,
-//         exclude: /node_modules/,
-//         use: ["style-loader", "css-loader"]
-//       }
-//     ]
-//   }
-// };
-var path = require("path");
+var path = require('path');
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "index.js",
-    libraryTarget: "commonjs2"
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, "src"),
+        include: path.resolve(__dirname, 'src'),
         exclude: /(node_modules|bower_components|build)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-react"]
+            presets: ['@babel/preset-react']
           }
         }
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
   externals: {
-    react: "commonjs react"
+    react: 'commonjs react'
   }
 };
